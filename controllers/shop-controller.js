26368fda23232ac2148c6077fe49237f42f9695d
Devil9xpro <chinhzz3107@gmail.com>
2021-07-26 16:29:08 +0700

    done crud with order mongoose

--- controllers/shop-controller.js
+++ controllers/shop-controller.js
@@ -98,14 +98,17 @@ exports.postOrder = (req, res, next) => {
             return order.save()
         })
         .then(result => {
-            res.redirect('/orders')
+            return req.user.clearCart()
         })
+        .then(() => {
+                res.redirect('/orders')
+            }
+        )
         .catch(err => console.log(err))
 };
 
 exports.getOrders = (req, res, next) => {
-    req.user
-        .getOrders()
+    Order.find({'user.userId': req.user._id})
         .then(orders => {
             res.render('shop/orders', {
                 path: '/orders',
