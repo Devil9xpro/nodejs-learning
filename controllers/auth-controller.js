078e8055be2db800effd056f33d49e43b88bed73
Nguyen-Van-Chinh <Nguyen.VanChinh@coxautoinc.com>
2021-08-09 22:45:54 +0700

    add login feature : compare hashed password with password in db

--- controllers/auth-controller.js
+++ controllers/auth-controller.js
@@ -20,14 +20,29 @@ exports.getSignup = (req, res, next) => {
 }
 
 exports.postLogin = (req, res, next) => {
-    User.findById('60fd30bacf328237e4c0b2e7')
+    const email = req.body.email
+    const password = req.body.password
+    User.findOne({email: email})
         .then(user => {
-            req.session.isLoggedIn = true
-            req.session.user = user
-            req.session.save((err) => {
-                console.log(err)
-                res.redirect('/')
-            })
+            if (!user) {
+                return res.redirect('/login')
+            }
+            bcrypt.compare(password, user.password)
+                .then(doMatch => {
+                    if (doMatch){
+                        req.session.isLoggedIn = true
+                        req.session.user = user
+                        return req.session.save((err) => {
+                            console.log(err)
+                            res.redirect('/')
+                        })
+                    }
+                    res.redirect('/login')
+                })
+                .catch(err => {
+                    console.log(err)
+                    res.redirect('/login')
+                })
         })
         .catch(err => console.log(err))
 }
@@ -50,17 +65,17 @@ exports.postSignup = (req, res, next) => {
                 return res.redirect('/signup')
             }
             return bcrypt.hash(password, 12)
-        })
-        .then(hashedPassword => {
-            const user = new User({
-                email: email,
-                password: hashedPassword,
-                cart: {items: []}
-            })
-            return user.save()
-        })
-        .then(result => {
-            res.redirect('/login')
+                .then(hashedPassword => {
+                    const user = new User({
+                        email: email,
+                        password: hashedPassword,
+                        cart: {items: []}
+                    })
+                    return user.save()
+                })
+                .then(result => {
+                    res.redirect('/login')
+                })
         })
         .catch(err => {
             console.log(err)
