9bc4dbf385e1f96c91ecf79945c05f7d806cfba1
Nguyen-Van-Chinh <Nguyen.VanChinh@coxautoinc.com>
2021-08-09 20:45:39 +0700

    encrypt password with bcrypt

--- controllers/auth-controller.js
+++ controllers/auth-controller.js
@@ -1,4 +1,5 @@
 const User = require('../models/user')
+const bcrypt = require('bcryptjs')
 
 exports.getLogin = (req, res, next) => {
     // const isLoggedIn = req.get('Cookie').split(';')[3].trim().split('=')[1]
@@ -48,9 +49,12 @@ exports.postSignup = (req, res, next) => {
             if (userDoc) {
                 return res.redirect('/signup')
             }
+            return bcrypt.hash(password, 12)
+        })
+        .then(hashedPassword => {
             const user = new User({
                 email: email,
-                password: password,
+                password: hashedPassword,
                 cart: {items: []}
             })
             return user.save()
