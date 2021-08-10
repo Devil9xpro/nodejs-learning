499cbd44c27b11ced067ca0210b9c456becd8e0f
Nguyen-Van-Chinh <Nguyen.VanChinh@coxautoinc.com>
2021-08-10 16:39:56 +0700

    add csrf protection: csrf token

--- app.js
+++ app.js
@@ -4,35 +4,42 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose')
 const session = require('express-session')
-const MongoDbStore = require('connect-mongodb-session')(session)
+const MongoDBStore = require('connect-mongodb-session')(session)
+const csrf = require('csurf')
 
-const errorController = require('./controllers/error-controller');
-const User = require('./models/user');
+const errorController = require('./controllers/error-controller')
+const User = require('./models/user')
 
 const MongoDB_URI = 'mongodb://localhost:27017/shop'
 
 const app = express();
-const store = new MongoDbStore({
+const store = new MongoDBStore({
     uri: MongoDB_URI,
-    collection: 'session'
-})
+    collection: 'sessions'
+});
+const csrfProtection = csrf()
 
-app.set('view engine', 'ejs');
-app.set('views', 'views');
+app.set('view engine', 'ejs')
+app.set('views', 'views')
 
-const adminRoutes = require('./routes/admin-router');
-const shopRoutes = require('./routes/shop-router');
-const authRoutes = require('./routes/auth-router');
+const adminRoutes = require('./routes/admin-router')
+const shopRoutes = require('./routes/shop-router')
+const authRoutes = require('./routes/auth-router')
 
-
-app.use(bodyParser.urlencoded({
-    extended: false
-}));
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(session({secret: 'my secret', resave: false, saveUninitialized: false, cookie: {}, store: store}))
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(express.static(path.join(__dirname, 'public')))
+app.use(
+    session({
+        secret: 'my secret',
+        resave: false,
+        saveUninitialized: false,
+        store: store
+    })
+);
+app.use(csrfProtection);
 
 app.use((req, res, next) => {
-    if(!req.session.user){
+    if (!req.session.user) {
         return next()
     }
     User.findById(req.session.user._id)
@@ -40,8 +47,14 @@ app.use((req, res, next) => {
             req.user = user
             next()
         })
-        .catch(err => console.log(err))
+        .catch(err => console.log(err));
 })
+
+app.use((req, res, next) => {
+    res.locals.isAuthenticated = req.session.isLoggedIn
+    res.locals.csrfToken = req.csrfToken()
+    next();
+});
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
@@ -49,8 +62,11 @@ app.use(authRoutes);
 
 app.use(errorController.get404);
 
-mongoose.connect(MongoDB_URI)
+mongoose
+    .connect(MongoDB_URI)
     .then(result => {
-        app.listen(3000)
+        app.listen(3000);
     })
-    .catch(err => console.log(err))
+    .catch(err => {
+        console.log(err);
+    });
