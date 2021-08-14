078c65c0bafd61abfe6f4ed3f9ac10a4e37e4351
Nguyen-Van-Chinh <Nguyen.VanChinh@coxautoinc.com>
2021-08-14 14:53:50 +0700

    add reset password feature

--- models/user.js
+++ models/user.js
@@ -10,6 +10,8 @@ const userSchema = new Schema({
         type: String,
         required: true
     },
+    resetToken: String,
+    resetTokenExpiration: Date,
     cart: {
         items: [{
             productId: {type: Schema.Types.ObjectId, required: true, ref: 'Product'},
