eab40fb9670b66834b3043a0c34d9e278aa57897
Nguyen-Van-Chinh <Nguyen.VanChinh@coxautoinc.com>
2021-08-15 20:23:37 +0700

    add error handling for both sync and async

--- controllers/admin-controller.js
+++ controllers/admin-controller.js
@@ -51,7 +51,9 @@ exports.postAddProduct = (req, res, next) => {
             res.redirect('/admin/products');
         })
         .catch(err => {
-            console.log(err);
+            const error = new Error(err)
+            error.statusCode = 500
+            return next(error)
         });
 };
 
@@ -76,7 +78,7 @@ exports.getEditProduct = (req, res, next) => {
                 validationErrors: []
             });
         })
-        .catch(err => console.log(err));
+        .catch(err => res.redirect('/500'));
 };
 
 exports.postEditProduct = (req, res, next) => {
@@ -119,7 +121,7 @@ exports.postEditProduct = (req, res, next) => {
                 console.log('UPDATED PRODUCT!');
                 res.redirect('/admin/products');
             })
-    }).catch(err => console.log(err));
+    }).catch(err => res.redirect('/500'));
 };
 
 exports.getProducts = (req, res, next) => {
@@ -132,7 +134,7 @@ exports.getProducts = (req, res, next) => {
                 path: '/admin/products',
             });
         })
-        .catch(err => console.log(err));
+        .catch(err => res.redirect('/500'));
 };
 
 exports.postDeleteProduct = (req, res, next) => {
@@ -142,5 +144,5 @@ exports.postDeleteProduct = (req, res, next) => {
             console.log('DESTROYED PRODUCT');
             res.redirect('/admin/products');
         })
-        .catch(err => console.log(err));
+        .catch(err => res.redirect('/500'));
 };
